295c1570e85818251efc542da7798e551631259e
OCTAVIO MARTINEZ <OCTAVIO MARTINEZ@LAPTOP-B73NCMDC>
2024-11-24 16:47:35 -0300

    ANEXO AVANCE TALLER 10 PROTOCOLO COLABORATIVO UNIDAD 2

---
+++ Taller10/Ejercicio1/Prueba.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejercicio1;
+
+/**
+ *
+ * @author OCTAVIO MARTINEZ
+ */
+public class Prueba {
+    
+    public static void main(String[] args) {
+        
+        Persona persona = new Persona();
+        
+        persona.hacerPresentarse();
+        
+        Persona estudiante = new Estudiante();
+        
+        estudiante.hacerPresentarse();
+        
+        Persona profesor = new Profesor();
+        
+        profesor.hacerPresentarse();
+        
+        
+    }
+    
+}
---
+++ Taller10/Ejercicio3/Animal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejercicio3;
+
+/**
+ *
+ * @author OCTAVIO MARTINEZ
+ */
+public class Animal {
+    // EL ERROR DE COMPILACION SE DA AL MOMENTO
+     // DE COLOCARLES PARAMETROS AL METODO. 
+    public void hacerAlimentar(){
+        
+        System.out.println(" EL ANIMAL DEBE ALIMENTARSE. "); 
+    }
+    
+}
